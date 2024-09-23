07d7a4a4293c12561ff78f99edb6800ff629e930
Kelvin Martins <kelvin.martins@sempreceub.com>
2024-09-22 21:15:58 -0300

    Atividade 08 concluida

--- Atividades-Monitoria-Py/Atividade08/Exercicio_unico.py
+++ Atividades-Monitoria-Py/Atividade08/Exercicio_unico.py
@@ -9,20 +9,21 @@ class Livro:
     def info(self):
         disponibilidade_str = "Disponível" if self.disponibilidade else "Indisponível"
         print(f"Título: {self.titulo} \nAutor: {self.autor}\nPublicação: {self.ano_pub}\nGênero: {self.genero}\nDisponibilidade: {str(disponibilidade_str)}")
+
     def emprestimo(self):
         if self.disponibilidade == False:
             print(f"O livro {self.titulo} foi emprestado")
         else:
-            print(f"O livro {self.titulo} está disponivel")
+            print(f"O livro {self.titulo} está disponivel para ser emprestado")
+
     def devolucao(self):
-        if self.disponibilidade == True:
+        if not self.disponibilidade:
+            self.disponibilidade = True
             print(f"O livro {self.titulo} foi devolvido")
-        else: print(f"O livro {self.disponibilidade} não foi devolvido")
+        else: print(f"O livro {self.titulo} está disponível")
+
     def verificar_disp(self):
-        if self.disponibilidade == True:
-            print("O livro está disponivel para emprestimo")
-        else:
-            print("Livro indisponivel para emprestimo")
+        return "Disponível" if self.disponibilidade else "indisponivel"
 
 livro1 = Livro("O Hobbit", "JRR Tolkien", 1950, "Fantasia", True)
 Livro.info(livro1)
